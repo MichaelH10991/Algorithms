f42c4cdf8d72f7fe2c9bade29f50c2447aa57751
Michael Humphries <michaelhumphries1997@gmail.com>
2019-09-29 17:42:24 +0100

    fixing algorithm

--- staircase.js
+++ staircase.js
@@ -1,15 +1,14 @@
 let n = 4
-let steps = [1, 2]
-let sum = 0
-let counter = 0
 
-for(i = 0; i< n;i++){
-    for(j = 0;j<n;j++){
-        sum = steps[i]+steps[j]
-        if(sum === 4){
-            counter += 1
-        }
+let staircase = (n) => {
+    let a = 0,  b = 1, temp = 0
+    for(i = 0; i < n; i++){
+        temp = b
+        b = a + b
+        a = temp
     }
-    sum = 0
+    return b
+
 }
-console.log(counter)
+
+console.log(staircase(n))
