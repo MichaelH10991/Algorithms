e226ab56092de8d58050d65244ee0d1f28a7da3f
Michael Humphries <michaelhumphries1997@gmail.com>
2019-10-03 08:12:22 +0100

    making some progresss

--- romanToInt.js
+++ romanToInt.js
@@ -1,20 +1,24 @@
-
-
 let numerals = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
 
-let s = "IIV"
+let s = "IIIV"
 
 // if the modulus 5 is 0
 let romanToInt = function (s) {
+    let n = 0
     for (i in s) {
         let numeral = s[i]
         for (j in numerals) {
             if (numerals[j][0] === numeral) {
-                console.log(`the numeral: ${s[i]} the number: ${numerals[j][1]}`)
-
+                console.log(`iter: ${i}: the numeral: ${s[i]} the number: ${numerals[j][1]}, str.len ${s.length} ${i == s.length - 2}`)
+                if (i == s.length - 1) {
+                    n += numerals[j][1]
+                } else {
+                    n += -numerals[j][1]
+                }
             }
         }
     }
+    console.log(Math.abs(n))
 }
 
 romanToInt(s)
