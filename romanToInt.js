f768a4ae1b9bfe10bbaecbcbf531e3f94e644998
Michael Humphries <michaelhumphries1997@gmail.com>
2019-10-04 07:34:59 +0100

    finished roman to int

--- romanToInt.js
+++ romanToInt.js
@@ -1,24 +1,32 @@
-let numerals = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
-
-let s = "IIIV"
+let s = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII",]
 
 // if the modulus 5 is 0
 let romanToInt = function (s) {
+    let pairs = ["IV", "IX", "XL", "XC", "CD", "CM"]
     let n = 0
-    for (i in s) {
-        let numeral = s[i]
-        for (j in numerals) {
-            if (numerals[j][0] === numeral) {
-                console.log(`iter: ${i}: the numeral: ${s[i]} the number: ${numerals[j][1]}, str.len ${s.length} ${i == s.length - 2}`)
-                if (i == s.length - 1) {
-                    n += numerals[j][1]
-                } else {
-                    n += -numerals[j][1]
-                }
-            }
+    let prev = ""
+    for (item in s) {
+        n += getNumber(s[item])
+        let pair = prev + s[item]
+        if (pairs.includes(pair)) {
+            // need to subtract by magnitude of X2
+            n -= 2 * getNumber(prev)
         }
+        prev = s[item]
     }
-    console.log(Math.abs(n))
+    console.log(`${Math.abs(n)}: ${s}`)
 }
 
-romanToInt(s)
+let getNumber = (numeral) => {
+    let numerals = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
+    for (j in numerals) {
+        let key = numerals[j][0]
+        if (key === numeral) {
+            return numerals[j][1]
+        }
+    }
+}
+
+for (i = 0; i < s.length; i++) {
+    romanToInt(s[i])
+}
