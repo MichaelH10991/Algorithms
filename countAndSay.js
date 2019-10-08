ec00a8d5491a100acd36bc8cb50fba68863ac5ee
Michael Humphries <michaelhumphries1997@gmail.com>
2019-10-08 18:45:26 +0100

    countAndSay solved

--- countAndSay.js
+++ countAndSay.js
@@ -1,28 +1,28 @@
-x = 13112221
+let n = 8
 
-let countAndSay = (x) => {
-    x = x.toString()
-    let count = 0
-    let text = ""
-    // setting initial num we're looking at
-    let temp = x[0]
-    for (i = 0; i <= x.length; i++) {
-        // temp should be equal on the first run
-        if (temp === x[i]) {
-            // store the num it matched on so it will always match until changed later on when new number appears
-            temp = x[i]
-            count++
-            // new number has appeared so change temp and append count to text
-        } else if (temp != x[i]) {
-            // append count to text
-            text += count + temp
-            // set count to 1 because we've seen this new number
-            count + 1
-            //change temp to the new number
-            temp = x[i]
-        }
-    }
-    console.log(text)
+let countAndSay = (n) => {
+    getNext(n)
 }
 
-countAndSay(x)
+let getNext = (n) => {
+    let init = "1", text = ""
+    for (j = 0; j < n; j++) {
+        let temp = init[0], count = 0, text = ""
+        console.log(`init: ${init}`)
+        for (i = 0; i <= init.length; i++) {
+            if (temp === init[i]) {
+                temp = init[i]
+                count++
+            } else if (temp != init[i]) {
+                console.log(count + temp)
+                text += count + temp
+                count = 1
+                temp = init[i]
+            }
+        }
+        init = text
+    }
+    return text
+}
+
+console.log(countAndSay(n))
